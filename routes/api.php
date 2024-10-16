5a0ef0d02d7cb0d54e8d0782ab03c608a906021c
Angelo Barreto <angbarretos@gmail.com>
2024-10-16 15:58:21 -0300

    agrupando rota de dados do user

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,29 @@
 
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\QuoteController;
-use App\Models\Carrier;
-use App\Models\VtexShippingRate;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
 
 
+/**
+ * Estas rotas estão agrupadas pelo middleware  sanctum, então será
+ * necessário autenticaçao para poder acessa-las
+ */
 Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+
     Route::get('frete/cotacao', [QuoteController::class, 'getQuote']);
     Route::get('frete/cotacao/{quoteId}', [QuoteController::class, 'getQuoteById']);
     Route::get('frete/usuario/{userId}/cotacoes', [QuoteController::class, 'getQuoteByUserId']);
-
 });
 
