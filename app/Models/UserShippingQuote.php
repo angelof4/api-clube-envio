031ce4ed23377f83a03fb69c947daa30f6309685
Angelo Barreto <angbarretos@gmail.com>
2024-10-16 15:30:21 -0300

    Modificaçao na model para permitir criaçao em massa e um metodo para registrar historico de cotaçao do usuario

--- app/Models/UserShippingQuote.php
+++ app/Models/UserShippingQuote.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserShippingQuote extends Model
 {
+
+
+    protected $fillable = ['user_id', 'quote_value', 'shipping_method_id'];
+
     // Define o relacionamento com a model User
     public function user()
     {
@@ -18,9 +22,20 @@ class UserShippingQuote extends Model
         return $this->belongsTo(ShippingMethod::class, 'shipping_method_id', 'id');
     }
 
-    // Se houver um relacionamento com VtexShippingRates, adicione conforme necessário
-    public function vtexShippingRates()
+
+    public function scopeRegisterUserShippingRate($query, $userId, $quotas)
     {
-        return $this->hasMany(VtexShippingRate::class, 'quote_id', 'id');
+        foreach ($quotas as $key => $quota) {
+            $query->create([
+                'user_id' => $userId,
+                'shipping_method_id' => $quota->shippingMethod->id,
+                'quote_value' => $quota->value,
+            ]);
+        }
+    }
+
+    public function scopeGetQuoteByUserId($query, $userId)
+    {
+        return $query->where('user_id', $userId);
     }
 }
